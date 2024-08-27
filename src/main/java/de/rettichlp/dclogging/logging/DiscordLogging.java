2e2dc7582da00e6b535ee8d51592283bc434bd2a
RettichLP <rettichlp01@gmail.com>
2024-08-27 23:15:34 +0200

    Add java doc comments for DiscordLogging

--- src/main/java/de/rettichlp/dclogging/logging/DiscordLogging.java
+++ src/main/java/de/rettichlp/dclogging/logging/DiscordLogging.java
@@ -45,18 +45,48 @@ public class DiscordLogging {
         this.errorMessageTemplate = errorMessageTemplate;
     }
 
+    /**
+     * Logs a message at the specified level.
+     *
+     * @param message the message to log
+     * @param level   the level at which to log the message
+     */
     public void log(@NotNull String message, @NotNull Level level) {
         log(message, level, null, this.textChannelId);
     }
 
+    /**
+     * Logs a message and an optional throwable at the specified level.
+     *
+     * @param message   the message to log
+     * @param level     the level at which to log the message
+     * @param throwable an optional throwable to log
+     */
     public void log(@NotNull String message, @NotNull Level level, @Nullable Throwable throwable) {
         log(message, level, throwable, this.textChannelId);
     }
 
+    /**
+     * Logs a message at the specified level to a specific text channel.
+     *
+     * @param message       the message to log
+     * @param level         the level at which to log the message
+     * @param textChannelId the ID of the text channel to log the message to
+     */
     public void log(@NotNull String message, @NotNull Level level, @NotNull String textChannelId) {
         log(message, level, null, textChannelId);
     }
 
+    /**
+     * Logs a message and an optional throwable at the specified level to a specific text channel.
+     *
+     * @param message       the message to log
+     * @param level         the level at which to log the message
+     * @param throwable     an optional throwable to log
+     * @param textChannelId the ID of the text channel to log the message to
+     *
+     * @throws IllegalStateException if the text channel cannot be found
+     */
     public void log(@NotNull String message, @NotNull Level level, @Nullable Throwable throwable, @NotNull String textChannelId) throws IllegalStateException {
         switch (level) {
             case INFO -> info(message, textChannelId);
@@ -65,30 +95,79 @@ public class DiscordLogging {
         }
     }
 
+    /**
+     * Logs an informational message.
+     *
+     * @param message the message to log
+     * @param args    optional arguments to format the message
+     */
     public void info(@NotNull String message, Object... args) {
         info(populate(message, args), this.textChannelId);
     }
 
+    /**
+     * Logs an informational message to a specific text channel.
+     *
+     * @param message       the message to log
+     * @param textChannelId the ID of the text channel to log the message to
+     */
     public void info(@NotNull String message, @NotNull String textChannelId) {
         send(textChannelId, this.infoMessageTemplate.applyMessage(message));
     }
 
+    /**
+     * Logs a warning message.
+     *
+     * @param message the message to log
+     * @param args    optional arguments to format the message
+     */
     public void warn(@NotNull String message, Object... args) {
         warn(populate(message, args), this.textChannelId);
     }
 
+    /**
+     * Logs a warning message to a specific text channel.
+     *
+     * @param message       the message to log
+     * @param textChannelId the ID of the text channel to log the message to
+     */
     public void warn(@NotNull String message, @NotNull String textChannelId) {
         send(textChannelId, this.warnMessageTemplate.applyMessage(message));
     }
 
+    /**
+     * Logs an error message.
+     *
+     * @param message the message to log
+     * @param args    optional arguments to format the message
+     *
+     * @throws IllegalStateException if the text channel cannot be found
+     */
     public void error(@NotNull String message, Object... args) throws IllegalStateException {
         error(populate(message, args), null, this.textChannelId);
     }
 
+    /**
+     * Logs an error message and an optional throwable.
+     *
+     * @param message   the message to log
+     * @param throwable an optional throwable to log
+     *
+     * @throws IllegalStateException if the text channel cannot be found
+     */
     public void error(@NotNull String message, @Nullable Throwable throwable) throws IllegalStateException {
         error(message, throwable, this.textChannelId);
     }
 
+    /**
+     * Logs an error message and an optional throwable to a specific text channel.
+     *
+     * @param message       the message to log
+     * @param throwable     an optional throwable to log
+     * @param textChannelId the ID of the text channel to log the message to
+     *
+     * @throws IllegalStateException if the text channel cannot be found
+     */
     public void error(@NotNull String message, @Nullable Throwable throwable, @NotNull String textChannelId) throws IllegalStateException {
         TextChannel textChannel = textChannelId.isBlank() ? getGuild().getSystemChannel() : getTextChannel(textChannelId);
         MessageCreateAction messageCreateAction = ofNullable(textChannel)
@@ -102,12 +181,27 @@ public class DiscordLogging {
         messageCreateAction.queue();
     }
 
+    /**
+     * Retrieves the guild associated with the bot.
+     *
+     * @return the Guild object
+     *
+     * @throws IllegalStateException if the bot is not a member of the guild
+     */
     @NotNull
     private Guild getGuild() throws IllegalStateException {
         return ofNullable(this.jda.getGuildById(this.guildId))
                 .orElseThrow(() -> new IllegalStateException("Bot is not a member in guild with id '" + this.guildId + "'"));
     }
 
+    /**
+     * Sends a message to a specified text channel.
+     *
+     * @param textChannelId the ID of the text channel to send the message to
+     * @param message       the message to send
+     *
+     * @throws IllegalStateException if the text channel cannot be found
+     */
     private void send(@NotNull String textChannelId, @NotNull String message) throws IllegalStateException {
         TextChannel textChannel = textChannelId.isBlank() ? getGuild().getSystemChannel() : getTextChannel(textChannelId);
 
@@ -117,11 +211,24 @@ public class DiscordLogging {
                 .queue();
     }
 
+    /**
+     * Populates a template string with the provided arguments.
+     *
+     * @param template the template string
+     * @param args     the arguments to populate the template with
+     *
+     * @return the populated string
+     */
     private String populate(String template, Object[] args) {
         return Stream.of(args)
                 .reduce(template, (result, arg) -> result.replaceFirst("\\{}", arg.toString()), (s1, s2) -> s1);
     }
 
+    /**
+     * Returns a new instance of the Builder class.
+     *
+     * @return a new Builder instance
+     */
     public static Builder getBuilder() {
         return new Builder();
     }
